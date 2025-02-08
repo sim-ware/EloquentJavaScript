437d9ada9ab580be7211d02d0fb404e9d92f5863
sim-ware <samir.gossain@gmail.com>
2025-02-08 16:30:52 +0000

    Part1:Chapter3:Functions - Complete

--- test.js
+++ test.js
@@ -1,11 +1,10 @@
-const size = 8;
-const hashRow = [];
-
-for (let count = 1; count <= size; count++) hashRow.push('# ');
-
-const hashRowString = hashRow.join('');
-
-for (let countTwo = 1; countTwo <= size; countTwo++) {
-  if (countTwo % 2 === 0) console.log(' ' + hashRowString);
-  else console.log(hashRowString);
+function countBs(string) {
+  let count = 0;
+  letters = string.split('');
+  
+  letters.forEach((letter) => {
+    if (letter === "B") count++;
+  });
 };
+
+console.log(countBs("BOB"));
