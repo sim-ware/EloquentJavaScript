f7b84096d01785a2c9ed3ac5b49af9e149e6505c
sim-ware <samir.gossain@gmail.com>
2025-02-11 16:50:23 +0000

    P1C5 - notes complete

--- test.js
+++ test.js
@@ -1,35 +1,8 @@
-function deepEqual(a, b) {
-  if (typeof a === 'object' && typeof b === 'object') {
-    const aKeys = Object.keys(a);
-    const bKeys = Object.keys(b);
+// Flattening
+// Use the reduce method in combination with the concat method to 
+// “flatten” an array of arrays into a single array that has all 
+// the elements of the original arrays.
 
-    for (let i = 0; i < aKeys.length; ++i) {
-      if (aKeys[i] !== bKeys[i]) return false;
-    };
-    
-    const aValues = Object.values(a);
-    const bValues = Object.values(b);
-
-    for (let i = 0; i < aValues.length; ++i) {
-      if (typeof aValues[i] === 'object' && typeof bValues[i] === 'object') 
-        return deepEqual(aValues[i], bValues[i]);
-      return aValues[i] == bValues[i];
-    };
-  };
-
-  return a == b;
-};
-
-//##Tests##//
-let obj = {
-  here: {
-    is: "an"
-  }, 
-  object: 2
-};
-console.log(deepEqual(obj, obj));
-// → true
-console.log(deepEqual(obj, {here: 1, object: 2}));
-// → false
-console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
-// → true
+let arrays = [[1, 2, 3], [4, 5], [6]];
+// Your code here.
+// → [1, 2, 3, 4, 5, 6]
